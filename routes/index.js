9ecb1fa2e00424381f455251344057ba4bbf9faf
Ana-stasi <anastasia.lapa@gmail.com>
2019-12-20 08:39:06 +0200

    minor fix

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,7 @@ router.get('/', function(req, res, next) {
   res.sendFile('../public/index.html');
 });
 router.get('/', function(req, res, next) {
-  res.sendFile('../public/file1.html');
-});
-router.get('/', function(req, res, next) {
-  res.sendFile('../public/file2.html');
-});
-router.get('/', function(req, res, next) {
-  res.sendFile('../public/file3.html');
-});
-router.get('/', function(req, res, next) {
   res.sendFile('../public/index.html');
-});
-router.get('/', function(req, res, next) {
-  res.sendFile('../public/lab.html');
 });
 router.get('/stylesheets/main.css', function(req, res) {
   res.sendFile(__dirname + "/" + "main.css");
